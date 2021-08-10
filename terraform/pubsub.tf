c6e3f4885191a32a7d48ae234e3a89616a1e7b15
Ben Nagy <nagy.benb@gmail.com>
2021-08-10 19:38:15 +0100

    add region to pubsub

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,6 +1,7 @@
 resource "google_pubsub_lite_topic" "garden_weather" {
   name = "villebon-garden-weather"
   project = data.google_project.project.number
+  region = var.region
 
   partition_config {
     count = 1
