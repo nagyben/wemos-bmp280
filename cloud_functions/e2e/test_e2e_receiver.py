61adaa5d51c5230b78453522156674a7d1cb5384
Ben Nagy <nagy.benb@gmail.com>
2021-08-14 13:56:04 +0100

    print e2e test response

--- cloud_functions/e2e/test_e2e_receiver.py
+++ cloud_functions/e2e/test_e2e_receiver.py
@@ -6,4 +6,5 @@ RECEIVER_URL = os.getenv("RECEIVER_URL")
 def test_receiver():
     response = requests.post(RECEIVER_URL, json={"key": "value"})
 
+    print(response.content)
     assert response.status_code == 200
