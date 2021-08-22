25855775f450d72f99752739b578b938f22b6564
Ben Nagy <nagy.benb@gmail.com>
2021-08-22 19:15:52 +0100

    add iam role for iot device

---
+++ terraform/iam.tf
@@ -0,0 +1,9 @@
+# https://github.com/hashicorp/terraform-provider-google/issues/400#issuecomment-360565229
+
+resource "google_service_account" "esp8266_sa" {
+  account_id   = "esp8266-sa"
+  display_name = "Service Account for ESP8266 IoT Device"
+}
+resource "google_service_account_key" "esp8266_key" {
+  service_account_id = "${google_service_account.esp8266_sa.id}"
+}
---
+++ terraform/secrets.tf
@@ -0,0 +1,13 @@
+resource "google_secret_manager_secret" "esp8266_pk" {
+  secret_id = "esp8266-pk"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "esp8266_pk_version" {
+  secret = google_secret_manager_secret.esp8266_pk.id
+
+  secret_data = google_service_account_key.esp8266_key.private_key
+}
