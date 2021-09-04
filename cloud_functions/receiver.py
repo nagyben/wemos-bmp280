f126f98d103498b90edc49074ac02c633d758163
Ben Nagy <nagy.benb@gmail.com>
2021-09-04 13:34:22 +0100

    use print for logging

--- cloud_functions/receiver.py
+++ cloud_functions/receiver.py
@@ -2,15 +2,7 @@ import logging
 import os
 import unittest.mock as mock
 
-import google.cloud.logging
 from firebase_admin import firestore
-from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
-
-client = google.cloud.logging.Client()
-handler = CloudLoggingHandler(client)
-logging.getLogger().setLevel(logging.DEBUG)  # defaults to WARN
-setup_logging(handler)
-LOG = logging.getLogger(__name__)
 
 
 def receiver(request):
@@ -23,8 +15,8 @@ def receiver(request):
         `make_response <https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.make_response>`.
     """
     db = _get_firestore_client()
-    LOG.debug(request.json)
-    LOG.debug("Inserting shtuff")
+    print(request.json)
+    print("Inserting shtuff")
     db.collection("weather").document("test").set({"key": "value"})
 
     return f"OK"
