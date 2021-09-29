31d16aadec2f892e8bca584dd444194026a19efd
Ben Nagy <nagy.benb@gmail.com>
2021-09-29 15:46:37 +0100

    clean up jwt test

--- weather-station-gcp/test/test_jwt.cpp
+++ weather-station-gcp/test/test_jwt.cpp
@@ -47,8 +47,9 @@ void test_signature(void) {
 
 
 void test_getJwt(void) {
-    const unsigned char header[] = "{\"alg\": \"RS256\",\"typ\": \"JWT\"}";
     const char payload[] = "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true,\"iat\":1516239022}";
+
+    // the expected is a RS256 JWT signed using the private key - see jwt.io
     const char expected[] = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ";
     char actual[1024];
     getJWT(payload, sizeof(payload), TEST_PEMKEY, actual);
