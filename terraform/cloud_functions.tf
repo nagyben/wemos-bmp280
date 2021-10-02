df0385ad598c307b33a2de20e058b62b33b313b4
Ben Nagy <nagy.benb@gmail.com>
2021-10-02 16:20:14 +0100

    add authenticated cf

--- terraform/cloud_functions.tf
+++ terraform/cloud_functions.tf
@@ -38,6 +38,27 @@ resource "google_cloudfunctions_function_iam_member" "receiver_function_invoker"
   region         = google_cloudfunctions_function.receiver_function.region
   cloud_function = google_cloudfunctions_function.receiver_function.name
 
-  role       = "roles/cloudfunctions.invoker"
-  member     = "allUsers"
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_cloudfunctions_function" "receiver_function_authenticated" {
+  name    = "receiver_authenticated"
+  runtime = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.functions_storage_bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
+  trigger_http          = true
+  entry_point           = "receiver_function"
+  max_instances         = 1
+}
+
+resource "google_cloudfunctions_function_iam_member" "receiver_function_authenticated_invoker" {
+  project        = google_cloudfunctions_function.receiver_function_authenticated.project
+  region         = google_cloudfunctions_function.receiver_function_authenticated.region
+  cloud_function = google_cloudfunctions_function.receiver_function_authenticated.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_service_account.esp8266_sa.email}"
 }
