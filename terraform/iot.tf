5bd62a194cf1f5c17ff2f93b800d9dbbea8e81af
Ben Nagy <nagy.benb@gmail.com>
2021-11-26 15:18:27 +0000

    move iot core to europe-west1

--- terraform/iot.tf
+++ terraform/iot.tf
@@ -1,6 +1,8 @@
 resource "google_cloudiot_registry" "cloudiot_registry" {
   name     = "iot-registry"
 
+  location = "europe-west1"
+
   event_notification_configs {
     pubsub_topic_name = google_pubsub_topic.pubsub_weather_topic.id
     subfolder_matches = ""
