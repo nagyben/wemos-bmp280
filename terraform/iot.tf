950b8e0bc6b0bdd9de1001787258dc740c5d581e
Ben Nagy <nagy.benb@gmail.com>
2021-11-26 15:23:50 +0000

    rename location to region

--- terraform/iot.tf
+++ terraform/iot.tf
@@ -1,7 +1,7 @@
 resource "google_cloudiot_registry" "cloudiot_registry" {
   name     = "iot-registry"
 
-  location = "europe-west1"
+  region = "europe-west1"
 
   event_notification_configs {
     pubsub_topic_name = google_pubsub_topic.pubsub_weather_topic.id
