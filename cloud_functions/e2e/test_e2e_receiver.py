4a523949717965d5c74f0f9e3499537771380cdb
Ben Nagy <nagy.benb@gmail.com>
2021-12-13 17:35:26 +0000

    authenticate e2e test before invoking function

--- cloud_functions/e2e/test_e2e_receiver.py
+++ cloud_functions/e2e/test_e2e_receiver.py
@@ -1,5 +1,8 @@
 import os
 
+import google.auth.transport.requests
+import google.oauth2.id_token
+import pytest
 import requests
 from google.cloud import firestore
 
@@ -9,6 +12,12 @@ GOOGLE_PROJECT = os.getenv("GOOGLE_PROJECT")
 COLLECTION = "test_weather"
 
 
+@pytest.fixture(scope="session")
+def token():
+    auth_req = google.auth.transport.requests.Request()
+    return google.oauth2.id_token.fetch_id_token(auth_req, RECEIVER_URL)
+
+
 def delete_documents(db: firestore.Client):
     docs = db.collection(COLLECTION).stream()
 
@@ -16,13 +25,15 @@ def delete_documents(db: firestore.Client):
         doc.reference.delete()
 
 
-def test_receiver():
+def test_receiver(token):
     db = firestore.Client(project=GOOGLE_PROJECT)
     delete_documents(db)
 
     new_doc = {"key": "value"}
 
-    response = requests.post(RECEIVER_URL, json=new_doc)
+    response = requests.post(
+        RECEIVER_URL, json=new_doc, headers={"Authorization": f"Bearer {token}"}
+    )
 
     print(response.content)
     assert response.status_code == 200
