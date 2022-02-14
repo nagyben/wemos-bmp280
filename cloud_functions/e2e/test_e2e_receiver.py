7588ededd43d2215b23616db6b43f250405e6a28
Ben Nagy <nagy.benb@gmail.com>
2022-02-14 23:27:50 +0000

    create pubsub triggered cloud function

--- cloud_functions/e2e/test_e2e_receiver.py
+++ cloud_functions/e2e/test_e2e_receiver.py
@@ -14,12 +14,6 @@ COLLECTION = "test_weather"
 
 
 @pytest.fixture(scope="session")
-def token():
-    auth_req = google.auth.transport.requests.Request()
-    return google.oauth2.id_token.fetch_id_token(auth_req, RECEIVER_URL)
-
-
-@pytest.fixture(scope="session")
 def db():
     return firestore.Client(project=GOOGLE_PROJECT)
 
@@ -29,28 +23,6 @@ def delete_documents(db: firestore.Client):
 
     for doc in docs:
         doc.reference.delete()
-
-
-def test_receiver(token, db):
-    delete_documents(db)
-
-    new_doc = {"key": "value"}
-
-    response = requests.post(
-        RECEIVER_URL, json=new_doc, headers={"Authorization": f"Bearer {token}"}
-    )
-
-    print(response.content)
-    assert response.status_code == 200
-
-    docs = db.collection(COLLECTION).stream()
-
-    try:
-        actual = next(docs).to_dict()
-        print(actual)
-        assert actual == new_doc
-    except StopIteration:
-        raise LookupError(f"Could not find any records in {COLLECTION}")
 
 
 def test_mqtt(db):
--- cloud_functions/receiver.py
+++ cloud_functions/receiver.py
@@ -1,7 +1,8 @@
+import base64
 import datetime
+import json
 import logging
 import os
-import unittest.mock as mock
 
 from google.cloud import logging as cloudlogging
 
@@ -16,20 +17,43 @@ def _setup_logging():
 from firebase_admin import firestore
 
 
-def receiver(request):
-    """Responds to any HTTP request.
+def _get_firestore_client():
+    logging.debug("Getting firestore client...")
+    return firestore.Client()
+
+
+def receiver(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
     Args:
-        request (flask.Request): HTTP request object.
+         event (dict):  The dictionary with data specific to this type of
+                        event. The `@type` field maps to
+                         `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
+                        The `data` field maps to the PubsubMessage data
+                        in a base64-encoded string. The `attributes` field maps
+                        to the PubsubMessage attributes if any is present.
+         context (google.cloud.functions.Context): Metadata of triggering event
+                        including `event_id` which maps to the PubsubMessage
+                        messageId, `timestamp` which maps to the PubsubMessage
+                        publishTime, `event_type` which maps to
+                        `google.pubsub.topic.publish`, and `resource` which is
+                        a dictionary that describes the service API endpoint
+                        pubsub.googleapis.com, the triggering topic's name, and
+                        the triggering event type
+                        `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
     Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.make_response>`.
+        None. The output is written to Cloud Logging.
     """
+
     _setup_logging()
     db = _get_firestore_client()
-    logging.debug(request.json)
+    mqtt_message = base64.b64decode(event["data"]).decode("utf-8")
+    logging.debug(context)
+    logging.debug(event)
+    logging.debug(mqtt_message)
 
-    doc = db.collection("weather").document(datetime.datetime.now().strftime("%Y%m%d"))
+    return b"OK"
+
+    doc = db.collection(COLLECTION).document(datetime.datetime.now().strftime("%Y%m%d"))
 
     if not doc.get().exists:
         doc.set(
@@ -42,12 +66,12 @@ def receiver(request):
         )
     else:
         data = doc.get().to_dict()["data"]
-        data.append({"timestamp": datetime.datetime.now().isoformat(), **request.json})
+        data.append(
+            {
+                "timestamp": datetime.datetime.now().isoformat(),
+                **json.loads(mqtt_message),
+            }
+        )
         doc.update({"data": data})
 
     return f"OK"
-
-
-def _get_firestore_client():
-    logging.debug("Getting firestore client...")
-    return firestore.Client()
