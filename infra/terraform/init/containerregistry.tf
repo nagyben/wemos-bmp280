454b40f536b4bd7a2d72ecf38c4adce30235a038
Ben Nagy <nagy.benb@gmail.com>
2022-06-27 11:50:24 +0100

    use a list to grant artifact registry IAM roles in tf

--- infra/terraform/init/containerregistry.tf
+++ infra/terraform/init/containerregistry.tf
@@ -1,3 +1,9 @@
+locals {
+  ci_artifact_repo_roles = [
+    "roles/artifactregistry.reader",
+  ]
+}
+
 resource "google_artifact_registry_repository" "docker_repo_init" {
   provider = google-beta
   repository_id = "nagyben-cr"
@@ -8,9 +14,10 @@ resource "google_artifact_registry_repository" "docker_repo_init" {
 
 resource "google_artifact_registry_repository_iam_member" "ci" {
   provider = google-beta
+  count    = length(local.ci_artifact_repo_roles)
 
   location = google_artifact_registry_repository.docker_repo_init.location
   repository = google_artifact_registry_repository.docker_repo_init.name
-  role   = "roles/artifactregistry.reader"
+  role   = local.ci_artifact_repo_roles[count.index]
   member = "serviceAccount:${var.ci_sa}"
 }
