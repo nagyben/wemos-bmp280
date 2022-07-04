d87dd7d54cefab3aefb5be8509553d3d86ba31de
Ben Nagy <nagy.benb@gmail.com>
2022-07-04 11:21:24 +0000

    make static site bucket public

--- infra/terraform/app/storage.tf
+++ infra/terraform/app/storage.tf
@@ -16,3 +16,9 @@ resource "google_storage_bucket" "static_site" {
     not_found_page   = "404.html"
   }
 }
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "READER"
+  entity = "allUsers"
+}
